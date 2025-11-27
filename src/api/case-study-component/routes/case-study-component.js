7a01a01c66d56115555fa992fd228852523ed217
vparmar2003 <vparmar@technithunder.com>
2025-11-27 15:47:12 +0530

    casestudy schema create

---
+++ src/api/case-study-component/routes/case-study-component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * case-study-component router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::case-study-component.case-study-component');
---
+++ src/api/case-study-hero-component/controllers/case-study-hero-component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * case-study-hero-component controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::case-study-hero-component.case-study-hero-component');
