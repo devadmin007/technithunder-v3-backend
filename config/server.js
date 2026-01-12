00643856fb22ac2c70ac40e93fc4d2c38ff9d15a
devadmin007 <dev.admin@technithunder.com>
2026-01-12 14:50:57 +0530

    Fix: specific config for Strapi Cloud deployment

--- config/server.js
+++ config/server.js
@@ -15,7 +15,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL'), // Add this for Railway
+  url: env('PUBLIC_URL') || env('URL'), // Add this for Railway/Strapi Cloud
   app: {
     keys: env.array('APP_KEYS'),
   },
